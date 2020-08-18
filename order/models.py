2be885f56ff39257b260217bc55d7b080293df32
koreicnurs <koreicnurs@gmail.com>
2020-08-18 11:59:49 +0600

    add full app accoutn, app models order and product

---
+++ order/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from product.models import Product
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='order_items')
+
+
+class Order(models.Model):
+    items = models.ManyToManyField(OrderItem)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    ordered_at = models.DateTimeField()
+
+#TODO status
---
+++ product/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+#TODO products list
+#TODO product details
+#TODO action(add_to_cart)
